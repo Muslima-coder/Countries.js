e6f92085f67f5fcdb759f96dfef6b18003651d17
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-05-23 16:22:05 +0500

    update commit

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ let elSearchInput = document.querySelector(".search-input")
 let elDarkMode = document.querySelector('#darkMBtn');
 let elLikeCount = document.querySelector(".like-count")
 let elSaveCount = document.querySelector(".save-count")
+let elModalWrapper = document.querySelector(".modal-wrapper")
+let elModalInner = document.querySelector(".modal-inner")
 
 
 let likeList = []
@@ -39,18 +41,7 @@ function renderProduct (arr, list){
        <div class="flex justify-between px-[24px] pb-[20px]">
        <button onclick="handleLikeClick(${item.id})" class=" ${item.isLiked ? "bg-red-500 text-white border-red-500" : "border-slate-500"} text-[var(--color-text)] border-[1.5px]  rounded-md w-[30%] cursor-pointer hover:w-[31%]  hover:border-blue-500 hover:text-blue-500 duration-300">Like</button>
        <button onclick="handleSaveClick(${item.id})" class=" ${item.isSaved ? "bg-purple-500 text-white border-purple-500" : "border-slate-500"} text-[var(--color-text)] border-[1.5px]  rounded-md w-[30%] cursor-pointer hover:w-[31%]  hover:border-blue-500 hover:text-blue-500 duration-300">Save</button>
-       
-
-       <button class="more-btn text-[var(--color-text)] border-[1.5px] border-slate-500 rounded-md w-[30%] cursor-pointer hover:w-[31%]  hover:border-blue-500 hover:text-blue-500 duration-300">More</button>
-<div class="modal-container hidden">
-  <div class="modal ">
-    <p class="text-[16px] text-[var(--color-text)]"><strong>Name:</strong> <span class="text-[14px] text-[var(--color-text)]">${item.name}</span></p>
-    <p class="text-[16px] text-[var(--color-text)]"><strong>Population:</strong> <span class="text-[14px] text-[var(--color-text)]">${item.population.toLocaleString("ru-RU")}</span></p>
-    <p class="text-[16px] text-[var(--color-text)]"><strong>Capital:</strong> <span class="text-[14px] text-[var(--color-text)]">${item.capital}</span></p>
-    <p class="text-[16px] text-[var(--color-text)]"><strong>Region:</strong> <span class="text-[14px] text-[var(--color-text)]">${item.name}</span></p>
-    <button class="close-btn hidden mt-4 text-sm bg-red-500 border border-transparent text-white px-3 py-1 rounded-[5px] hover:bg-transparent hover:border-red-500 hover:text-red-500 duration-300">Close</button>
-  </div>
-</div>
+       <button onclick="handleMoveBtnClick(${item.id})" class="more-btn text-[var(--color-text)] border-[1.5px] border-slate-500 rounded-md w-[30%] cursor-pointer hover:w-[31%]  hover:border-blue-500 hover:text-blue-500 duration-300">More</button>
         `
         list.appendChild(elItem)
     })
@@ -112,20 +103,24 @@ function handleSaveBtnClick() {
 
 
 //More btn 
-let moreButtons = document.querySelectorAll(".more-btn");
-let closeButtons = document.querySelectorAll(".close-btn");
-let modals = document.querySelectorAll(".modal-container");
-
-moreButtons.forEach((btn, item) => {
-  btn.addEventListener("click", () => {
-    modals[item].classList.remove("hidden");
-    closeButtons[item].classList.remove("hidden");
-  });
-});
-
-closeButtons.forEach((btn, item) => {
-  btn.addEventListener("click", () => {
-    modals[item].classList.add("hidden");
-    btn.classList.add("hidden");
-  });
-});
+function handleMoveBtnClick (id){
+  elModalWrapper.classList.remove("scale-0")
+  let findObj = countries.find(item => item.id == id)
+  console.log(findObj)
+  elModalInner.innerHTML = `
+  <div class="flex justify-between items-center">
+  <img class="w-[60%]" src="${findObj.flag}" alt="Single flag" width="400" height="300" />
+  <div class="w-[40%] text-end">
+      <strong class="font-extrabold inline-block text-[18px] text-[var(--color-text)] mb-[16px]">${findObj.name}</strong>
+       <p class="text-[var(--color-text)]"> <span class="font-semibold text-[14px] leading-[16px] text-[var(--color-text)]">Population: </span> ${findObj.population.toLocaleString("ru-RU")}</p>
+      <p class="text-[var(--color-text)]"> <span class="font-semibold text-[14px] leading-[16px] text-[var(--color-text)]">Region: </span> ${findObj.name}</p>
+      <p class="text-[var(--color-text)]"> <span class="font-semibold text-[14px] leading-[16px] text-[var(--color-text)]">Capital: </span> ${findObj.capital}</p>
+  </div>
+  </div>
+  `
+}
+elModalWrapper.addEventListener("click", function(e){
+  if(e.target.id){
+    elModalWrapper.classList.add("scale-0")
+  }
+})
